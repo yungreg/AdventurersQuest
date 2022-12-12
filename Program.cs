8da58a0974ffeebb34459372379efbe7cca9a783
Reginald Williams Jr <rlwjr433@gmail.com>
2022-12-11 22:04:56 -0600

    Phase 2 done

--- Program.cs
+++ Program.cs
@@ -14,6 +14,13 @@ namespace Quest
             //   the text of the challenge
             //   a correct answer
             //   a number of awesome points to gain or lose depending on the success of the challenge
+
+            //Phase 2: 
+            // todo get user name
+            // todo: send it to adventurer constructor's Name Fielkd
+            Console.WriteLine("Before we begin, enter your name:");
+            string userAnswer = Console.ReadLine();
+
             Challenge twoPlusTwo = new Challenge("2 + 2?", 4, 10);
             Challenge theAnswer = new Challenge(
                 "What's the answer to life, the universe and everything?", 42, 25);
@@ -43,7 +50,9 @@ namespace Quest
             int maxAwesomeness = 100;
 
             // Make a new "Adventurer" object using the "Adventurer" class
-            Adventurer theAdventurer = new Adventurer("Jack");
+
+
+            Adventurer theAdventurer = new Adventurer($"{userAnswer}");
 
             // A list of challenges for the Adventurer to complete
             // Note we can use the List class here because have the line "using System.Collections.Generic;" at the top of the file.
